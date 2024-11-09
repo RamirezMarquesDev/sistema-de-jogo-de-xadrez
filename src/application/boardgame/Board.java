9c9d34d1eb1d42a74d49c13484218ec551c9f7af
RAMIREZ MARQUES DE ANDRADE CAMPELLO <129707429+RamirezMarquesDev@users.noreply.github.com>
2024-11-09 16:22:43 -0300

    Implementado a lógica de check

--- src/application/boardgame/Board.java
+++ src/application/boardgame/Board.java
@@ -47,7 +47,7 @@ public class Board {
         piece.position = position;
     }
 
-    public Piece removPiece(Position position) {
+    public Piece removePiece(Position position) {
         if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
