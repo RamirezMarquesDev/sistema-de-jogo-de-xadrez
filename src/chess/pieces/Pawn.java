48b8b87fb9bdcb5299f3cfd6fe01a1f07e960fd7
RAMIREZ MARQUES DE ANDRADE CAMPELLO <129707429+RamirezMarquesDev@users.noreply.github.com>
2024-11-09 17:42:42 -0300

    Implementado a Classe Queen (Rainha) & sua lógica de movimentos

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -47,7 +47,7 @@ public class Pawn extends ChessPiece {
             }
             // Teste se o pião tem contagem zero e pode mover 2 casas cor preta
             p.setValues(position.getRow() + 2, position.getColumn());
-            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
                     && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColumn()] = true;
